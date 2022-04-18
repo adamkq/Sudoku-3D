e7cd0f00039d83f5025a63cbceb5ac1ece4769f2
Adam K-Q <adamkq@icloud.com>
2022-04-17 18:44:52 -0700

    Continue work on menus

--- Assets/Scripts/UserInteraction/InGameOptionsMenuManager.cs
+++ Assets/Scripts/UserInteraction/InGameOptionsMenuManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class InGameOptionsMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject m_optionsMenu;
     [SerializeField] private GameObject m_confirmExitDialog;
     [SerializeField] private GameObject m_confirmResetDialog;
     [SerializeField] private GameObject m_guidePage;
@@ -14,10 +15,17 @@ public class InGameOptionsMenuManager : MonoBehaviour
         m_masterController = GameObject.FindGameObjectWithTag("MasterController").GetComponent<MasterController>();
     }
 
+    public void ShowOptionsMenu(bool show)
+    {
+        m_optionsMenu.SetActive(show);
+    }
+
     public void CloseDialogOrPage()
     {
         // close any dialogs or pages opened by this script
-
+        if (m_confirmExitDialog) m_confirmExitDialog.SetActive(false);
+        if (m_confirmResetDialog) m_confirmResetDialog.SetActive(false);
+        if (m_guidePage) m_guidePage.SetActive(false);
     }
 
     public void OpenDialog(GameObject dialog)
